71fd8359062ad1ae5dc53e115665a0828d281c49
youyanling <3219402965@qq.com>
2020-04-21 12:01:55 +0800

    4/21

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180117/Ch10Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180117/Ch10Activity1.java
@@ -7,12 +7,13 @@ import android.util.Log;
 import android.view.View;
 
 public class Ch10Activity1 extends AppCompatActivity {
-
+private  Integer count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_ch10_1);
         Log.i(Ch10Activity1.class.toString(),"onCreate");
+        count=0;
     }
 public void finishClick(View view){
     finish();//关闭界面
@@ -25,14 +26,25 @@ public void finishClick(View view){
         Log.i(Ch10Activity1.class.toString(),"onStart");
     }
 
+//计数的方法
+    public void counter (View view){
+        count++;
+        Log.i(Ch10Activity1.class.toString(),"counter:"+count);
+    }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        //使用本方法保存一些界面的状态信息
+        //数据保存到Bundle对象中
+        outState.putInt("count",count);
         super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        //恢复之前保存的状态信息
+        count=savedInstanceState.getInt("count");
+        Log.i(Ch10Activity1.class.toString(),"onRestoreInstanceState");
     }
 
     @Override
